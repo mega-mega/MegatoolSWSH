3f0bb3dda76d464c912f7fa22b9768a69b83bc15
shibuya <t.shibuya.uva@gmail.com>
2020-07-11 17:00:24 +0900

    dev

--- src/screens/PokeEditScreen.tsx
+++ src/screens/PokeEditScreen.tsx
@@ -1,74 +1,95 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+} from "react-native";
 
-const names = ["ガブリアス", "フライゴン", "ピカチュウ"];
+import StatusTable from "../components/StatusTable";
+import { Banner } from "../elements/Banner";
+
 export const PokeEditScreen = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>name: </Text>
-        <TextInput
-          style={styles.nameInput}
-          onChangeText={(text) => console.log(text)}
-        />
-      </View>
-      <View>
-        <Text>ニックネーム: </Text>
-        <TextInput
-          style={styles.nameInput}
-          onChangeText={(text) => console.log(text)}
-        />
-      </View>
-      <View style={{ width: "100%", flexDirection: "row" }}>
-        <View style={{ width: "50%", paddingRight: 5 }}>
-          <Text>特性: </Text>
+      <ScrollView style={styles.scroll}>
+        <View>
+          <Text>name: </Text>
           <TextInput
-            style={styles.abilityInput}
+            style={styles.nameInput}
             onChangeText={(text) => console.log(text)}
           />
         </View>
-        <View style={{ width: "50%", paddingLeft: 5 }}>
-          <Text>持ち物: </Text>
+        <View>
+          <Text>ニックネーム: </Text>
           <TextInput
-            style={styles.abilityInput}
+            style={styles.nameInput}
             onChangeText={(text) => console.log(text)}
           />
         </View>
-      </View>
-      {/* 技エリア */}
-      <View style={{ width: "100%", flexDirection: "row" }}>
-        <View style={{ width: "50%", paddingRight: 5 }}>
-          <Text>わざ1: </Text>
+        <View style={{ width: "100%", flexDirection: "row" }}>
+          <View style={{ width: "50%", paddingRight: 5 }}>
+            <Text>特性: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+          <View style={{ width: "50%", paddingLeft: 5 }}>
+            <Text>持ち物: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+        </View>
+        {/* 技エリア */}
+        <View style={{ width: "100%", flexDirection: "row" }}>
+          <View style={{ width: "50%", paddingRight: 5 }}>
+            <Text>わざ1: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+          <View style={{ width: "50%", paddingLeft: 5 }}>
+            <Text>わざ2: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+        </View>
+        <View style={{ width: "100%", flexDirection: "row" }}>
+          <View style={{ width: "50%", paddingRight: 5 }}>
+            <Text>わざ3: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+          <View style={{ width: "50%", paddingLeft: 5 }}>
+            <Text>わざ4: </Text>
+            <TextInput
+              style={styles.abilityInput}
+              onChangeText={(text) => console.log(text)}
+            />
+          </View>
+        </View>
+        <StatusTable />
+        <View
+          style={{ marginTop: 10, backgroundColor: "gray", height: "100%" }}
+        >
+          <Text>備考: </Text>
           <TextInput
-            style={styles.abilityInput}
+            style={styles.nameInput}
             onChangeText={(text) => console.log(text)}
           />
         </View>
-        <View style={{ width: "50%", paddingLeft: 5 }}>
-          <Text>わざ2: </Text>
-          <TextInput
-            style={styles.abilityInput}
-            onChangeText={(text) => console.log(text)}
-          />
-        </View>
-      </View>
-      <View style={{ width: "100%", flexDirection: "row" }}>
-        <View style={{ width: "50%", paddingRight: 5 }}>
-          <Text>わざ3: </Text>
-          <TextInput
-            style={styles.abilityInput}
-            onChangeText={(text) => console.log(text)}
-          />
-        </View>
-        <View style={{ width: "50%", paddingLeft: 5 }}>
-          <Text>わざ4: </Text>
-          <TextInput
-            style={styles.abilityInput}
-            onChangeText={(text) => console.log(text)}
-          />
-        </View>
-      </View>
+      </ScrollView>
+      <Banner style={styles.banner} />
     </View>
   );
 };
@@ -76,22 +97,30 @@ export const PokeEditScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
+  },
+  scroll: {
     width: "90%",
     paddingTop: 10,
+    flex: 1,
     marginRight: "auto",
     marginLeft: "auto",
   },
   nameInput: {
-    height: 40,
+    height: 30,
     width: "100%",
     borderColor: "gray",
     borderWidth: 1,
   },
   abilityInput: {
-    height: 40,
+    height: 30,
     width: "100%",
     borderColor: "gray",
     borderWidth: 1,
+  },
+  banner: {
+    position: "absolute",
+    bottom: 0,
   },
 });
 
