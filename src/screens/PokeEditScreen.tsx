e7a5ee96657e47b7515b80ec73f9ee960f287e0c
mega_megai <34888476+mega-mega@users.noreply.github.com>
2020-07-12 17:48:29 +0900

    個体保存機能の作成 (#4)

--- src/screens/PokeEditScreen.tsx
+++ src/screens/PokeEditScreen.tsx
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  ScrollView,
-  TextInput,
-} from "react-native";
+import React from "react";
+import { StyleSheet, View, Text, ScrollView, TextInput } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import StatusTable from "../components/StatusTable";
@@ -14,36 +7,52 @@ import { Banner } from "../elements/Banner";
 import { PokeType } from "../../common/PokeType";
 import AppState from "../states/AppState";
 import State from "../State";
-import {
-  createUpdateMessageAction,
-  createUpdatePokeAction,
-} from "../actions/AppAction";
+import { createUpdatePokeAction } from "../actions/AppAction";
 
 interface Events {
   // ポケモンリストと1個体分のイベント作る
-  onChangeMessage: (message: string) => void;
+
   onChangePokemon: (pokeData: PokeType) => void;
 }
 
-interface Props extends Events, AppState {}
+interface Props extends Events, AppState {
+  pokeProp?: PokeType;
+}
 
 export const PokeEditScreen = (props: Props) => {
-  // const [poke, setPoke] = useState(props.pokeData);
-  const pokeData: PokeType = {
+  const pokeData: PokeType = props.pokeProp || {
     number: 0,
     name: "ガブリアス",
-    nn: "",
+    nn: "陽気スカーフ",
     ability: "さめはだ",
     pokesonality: "ようき",
     item: "こだわりスカーフ",
+    waza: ["", "", "", ""],
     status: {
       bs: [0, 0, 0, 0, 0, 0],
       iv: [0, 0, 0, 0, 0, 0],
       ev: [0, 0, 0, 0, 0, 0],
       st: [0, 0, 0, 0, 0, 0],
     },
+    memo: "",
+    createAt: new Date(),
+    updateAt: new Date(),
   };
-
+  // 技入力エリア1つ
+  const wazaInput = (waza: string, index: number) => {
+    return (
+      <View>
+        <Text>{waza}</Text>
+        <TextInput
+          style={styles.abilityInput}
+          onChangeText={(text) => {
+            pokeData.waza[index] = text;
+            props.onChangePokemon(pokeData);
+          }}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll}>
@@ -61,7 +70,10 @@ export const PokeEditScreen = (props: Props) => {
           <Text>ニックネーム: </Text>
           <TextInput
             style={styles.nameInput}
-            onChangeText={(text) => console.log(text)}
+            onChangeText={(text) => {
+              pokeData.nn = text;
+              props.onChangePokemon(pokeData);
+            }}
           />
         </View>
         <View style={{ width: "100%", flexDirection: "row" }}>
@@ -69,48 +81,38 @@ export const PokeEditScreen = (props: Props) => {
             <Text>特性: </Text>
             <TextInput
               style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
+              onChangeText={(text) => {
+                pokeData.ability = text;
+                props.onChangePokemon(pokeData);
+              }}
             />
           </View>
           <View style={{ width: "50%", paddingLeft: 5 }}>
             <Text>持ち物: </Text>
             <TextInput
               style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
+              onChangeText={(text) => {
+                pokeData.item = text;
+                props.onChangePokemon(pokeData);
+              }}
             />
           </View>
         </View>
         {/* 技エリア */}
         <View style={{ width: "100%", flexDirection: "row" }}>
           <View style={{ width: "50%", paddingRight: 5 }}>
-            <Text>わざ1: </Text>
-            <TextInput
-              style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
-            />
+            {wazaInput("わざ1: ", 0)}
           </View>
           <View style={{ width: "50%", paddingLeft: 5 }}>
-            <Text>わざ2: </Text>
-            <TextInput
-              style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
-            />
+            {wazaInput("わざ2: ", 1)}
           </View>
         </View>
         <View style={{ width: "100%", flexDirection: "row" }}>
           <View style={{ width: "50%", paddingRight: 5 }}>
-            <Text>わざ3: </Text>
-            <TextInput
-              style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
-            />
+            {wazaInput("わざ3: ", 2)}
           </View>
           <View style={{ width: "50%", paddingLeft: 5 }}>
-            <Text>わざ4: </Text>
-            <TextInput
-              style={styles.abilityInput}
-              onChangeText={(text) => console.log(text)}
-            />
+            {wazaInput("わざ4: ", 3)}
           </View>
         </View>
         <StatusTable />
@@ -120,10 +122,11 @@ export const PokeEditScreen = (props: Props) => {
           <Text>備考: </Text>
           <TextInput
             style={styles.nameInput}
-            onChangeText={(text) => console.log(text)}
-          >
-            {props.pokeData?.name}
-          </TextInput>
+            onChangeText={(text) => {
+              pokeData.memo = text;
+              props.onChangePokemon(pokeData);
+            }}
+          />
         </View>
       </ScrollView>
       <Banner style={styles.banner} />
@@ -170,9 +173,6 @@ const mapStateToProps = (state: State): AppState => {
 
 const mapDispatchToProps = (dispatch: Dispatch): Events => {
   return {
-    onChangeMessage: (message: string) => {
-      dispatch(createUpdateMessageAction(message));
-    },
     onChangePokemon: (pokeData: PokeType) => {
       dispatch(createUpdatePokeAction(pokeData));
     },
