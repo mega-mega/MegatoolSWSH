1200b96a38499a7f1c90152ebe9852858b5c447f
shibuya <t.shibuya.uva@gmail.com>
2020-08-16 17:59:37 +0900

    make stack paty navigator

--- src/screens/PartyHomeScreen.tsx
+++ src/screens/PartyHomeScreen.tsx
@@ -1,40 +1,50 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Header, Icon } from "react-native-elements";
 import palet from "../../common/palet.json";
-import PartyListScreen from "./PartyListScreen";
 import PartyChoiceScreen from "./PartyChoiceScreen";
-import { TouchableOpacity, StyleSheet } from "react-native";
+import PartyEditScreen from "./PartyEditScreen";
+import PartyListScreen from "./PartyListScreen";
 
 const Stack = createStackNavigator();
-
-export const PartyHomeScreen = () => {
+interface Props {}
+export const PartyHomeScreen = (props: Props) => {
   return (
     <Stack.Navigator screenOptions={headerOption}>
       <Stack.Screen
-        name="バトルボックス"
+        name="対戦準備"
         options={{
-          header: () => {
+          header: ({ scene, previous, navigation }) => {
             return (
               <Header
                 centerComponent={{
-                  text: "パーティ選択",
+                  text: "対戦準備",
                   style: { color: palet.text, fontSize: 16 },
                 }}
                 rightComponent={
                   <TouchableOpacity
                     onPress={() => {
-                      // TODO: パーティ選択画面に遷移、
-                      console.log("click");
+                      navigation.navigate("バトルボックス一覧");
                     }}
                   >
-                    <Icon
-                      name={"exchange-alt"}
-                      color={palet.text}
-                      size={20}
-                      type={"font-awesome-5"}
-                      iconStyle={styles.iconStyle}
-                    />
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Text style={[styles.text, { marginRight: 10 }]}>
+                        切替
+                      </Text>
+                      <Icon
+                        name={"exchange-alt"}
+                        color={palet.text}
+                        size={20}
+                        type={"font-awesome-5"}
+                        style={{ marginRight: 5 }}
+                      />
+                    </View>
                   </TouchableOpacity>
                 }
                 backgroundColor={palet.main}
@@ -47,7 +57,7 @@ export const PartyHomeScreen = () => {
       <Stack.Screen
         name="バトルボックス一覧"
         options={{
-          header: () => {
+          header: ({ scene, previous, navigation }) => {
             return (
               <Header
                 centerComponent={{
@@ -55,11 +65,52 @@ export const PartyHomeScreen = () => {
                   style: { color: palet.text, fontSize: 16 },
                 }}
                 backgroundColor={palet.main}
+                leftComponent={
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.goBack();
+                    }}
+                  >
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Icon
+                        name="angle-left"
+                        type="font-awesome"
+                        color={palet.text}
+                        style={{
+                          marginLeft: 10,
+                        }}
+                      />
+                      <Text style={styles.text}>一覧</Text>
+                    </View>
+                  </TouchableOpacity>
+                }
               />
             );
           },
         }}
         component={PartyListScreen}
+      />
+      <Stack.Screen
+        name="バトルボックス編集"
+        options={{
+          header: () => {
+            return (
+              <Header
+                centerComponent={{
+                  text: "バトルボックス編集",
+                  style: { color: palet.text, fontSize: 16 },
+                }}
+                backgroundColor={palet.main}
+              />
+            );
+          },
+        }}
+        component={PartyEditScreen}
       />
     </Stack.Navigator>
   );
@@ -82,6 +133,11 @@ const styles = StyleSheet.create({
   iconStyle: {
     color: palet.back,
     marginRight: 10,
+  },
+  text: {
+    marginLeft: 10,
+    fontSize: 16,
+    color: palet.text,
   },
 });
 export default PartyHomeScreen;
